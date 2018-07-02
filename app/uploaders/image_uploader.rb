40059307d4bae8814f0abff604419d69a0c0771f
tranhuyhoangbka <hoangth92.nd@gmail.com>
2018-07-02 18:45:43 +0700

    fix image uploader 2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,7 +20,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/default.png")
 
-    "public/preview_no_image.jpg"
+    "/preview_no_image.jpg"
   end
 
   # Process files as they are uploaded:
