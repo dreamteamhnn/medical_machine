ed7c55d5d3b0b4ca16b99531b05a2c4f9f9fe253
HUSTToanLH <laihuutoan@gmail.com>
2018-10-15 00:55:08 +0700

    Fixbug 14/10/2018

--- app/models/concerns/friendlyid_configuration.rb
+++ app/models/concerns/friendlyid_configuration.rb
@@ -2,7 +2,8 @@ concern :FriendlyidConfiguration do
   included do
     extend FriendlyId
 
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true
+    # validates :name, presence: true, uniqueness: true
 
     friendly_id :beauty_slug, use: :slugged
   end
