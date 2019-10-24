dcd0f3865854ace6e5bca3e144d9654458ae453a
Toan LH <toanlh@opengine.app>
2019-10-24 21:32:18 +0700

    Production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,4 +106,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.serve_static_assets = true
 end
