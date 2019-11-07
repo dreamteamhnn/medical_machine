b2839fd04b6ab6ff7e884979e1e12ab7672b61f4
Lai Huu Toan <huutoan.lai@vn.panasonic.com>
2019-11-07 16:25:44 +0700

    Add feedback model

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,7 @@
 class Feedback < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
+  BRAND_ATTRIBUTES = %i(name location description image home_order)
+
+  validates :location, presence: true
 end
