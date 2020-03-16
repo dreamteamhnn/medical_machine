ed0f16162b9aedc8eceb297a61dee61c72e49757
Toan LH <toanlh@opengine.app>
2020-03-16 11:51:17 +0700

    Mailer order

--- app/mailers/product_order_mailer.rb
+++ app/mailers/product_order_mailer.rb
@@ -1,7 +1,10 @@
 class ProductOrderMailer < ApplicationMailer
   def order user_info, product
     @user_info, @product = user_info, product
-    mail to: "thietbistech@gmail.com",
-      subject: "<Thiết bị y tế> Có một đơn hàng mới từ #{@user_info[:username]}"
+    mail to: "laihuutoan@gmail.com",
+      subject: "<Stech Sài Gòn> Có một đơn hàng mới từ #{@user_info[:username]}"
+
+    # mail to: @company.email,
+    #   subject: "<Stech Sài Gòn> Có một đơn hàng mới từ #{@user_info[:username]}"
   end
 end
