45f8244e704a495530d82886054ed6a1ee59bbea
Toàn Lại <toan.lai@reti.vn>
2020-10-06 00:43:05 +0700

    fix share blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  include ActionView::Helpers::SanitizeHelper
   before_action :load_blog, only: :show
   before_action :load_blogs, only: :index
   before_action :load_left_menu, only: [:show, :index]
