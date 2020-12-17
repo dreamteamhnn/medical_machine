dcd419ab616ce81668093106e5c9fcaee3422a53
Toàn Lại <toan.lai@reti.vn>
2020-12-17 10:58:00 +0700

    Change serve static assets to false on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -106,5 +106,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 end
